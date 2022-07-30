d3f6896aa558a61e1053c1209f1102f05b7b1064
ijoanet <jramos@thehotelsnetwork.com>
2022-07-30 18:30:12 +0200

    feat(ironhack): done

--- javascript/index.js
+++ javascript/index.js
@@ -3,38 +3,110 @@
 
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
-// Out of sync
-  getInstruction("mashedPotatoes", 0, (step1) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 1, (step2) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 2, (step3) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 3, (step4) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 4, (step5) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
-    document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-  }, (error) => console.log(error));
+const foodList = [
+    {
+        name: 'mashedPotatoes',
+        steps: 5,
+    },
+    {
+        name: 'steak',
+        steps: 8,
+    },
+    {
+        name: 'brusselsSprouts',
+        steps: 8,
+    },
+    {
+        name: 'broccoli',
+        steps: 7,
+    },
+]
 
+const printError = (err) => console.error(err);
 
 
 // Iteration 1 - using callbacks
-// ...
+// for (let food of foodList) {
+//     const printItem = (instruction) => {
+//         document.querySelector(`#${food.name}`).innerHTML += `<li>${instruction}</li>`;
+//     }
+//     const printLastItem = (instruction) => {
+//         document.querySelector(`#${food.name}`).innerHTML += `<li>${instruction}</li>`;
+//         document.querySelector(`#${food.name}Img`).removeAttribute("hidden");
+//     }
+//
+//     for (let step = 0; step < food.steps; step++) {
+//         if (step === food.steps - 1) {
+//             getInstruction(food.name, step, printLastItem, printError);
+//         } else {
+//             getInstruction(food.name, step, printItem, printError);
+//         }
+//     }
+//
+// }
+
 
 // Iteration 2 - using promises
-// ...
+// foodList.forEach(async (food) => {
+//     const printItem = (instruction) => {
+//         document.querySelector(`#${food.name}`).innerHTML += `<li>${instruction}</li>`;
+//     }
+//     const printLastItem = (instruction) => {
+//         document.querySelector(`#${food.name}`).innerHTML += `<li>${instruction}</li>`;
+//         document.querySelector(`#${food.name}Img`).removeAttribute("hidden");
+//     }
+//
+//     for (let step = 0; step < food.steps; step++) {
+//         if (step === food.steps - 1) {
+//             await obtainInstruction(food.name, step).then(printLastItem).catch(printError);
+//         } else {
+//             await obtainInstruction(food.name, step).then(printItem).catch(printError);
+//         }
+//     }
+// })
 
 // Iteration 3 using async/await
-// ...
+// const processFood = async function(food) {
+//     const printItem = (instruction) => {
+//         document.querySelector(`#${food.name}`).innerHTML += `<li>${instruction}</li>`;
+//     }
+//     const printLastItem = (instruction) => {
+//         document.querySelector(`#${food.name}`).innerHTML += `<li>${instruction}</li>`;
+//         document.querySelector(`#${food.name}Img`).removeAttribute("hidden");
+//     }
+//
+//     for (let step = 0; step < food.steps; step++) {
+//         if (step === food.steps - 1) {
+//             await obtainInstruction(food.name, step).then(printLastItem).catch(printError);
+//         } else {
+//             await obtainInstruction(food.name, step).then(printItem).catch(printError);
+//         }
+//     }
+// }
+// foodList.forEach(async (food) => {
+//     await processFood(food)
+// })
 
 // Bonus 2 - Promise all
-// ...
+foodList.forEach(async (food) => {
+    const printItem = (instruction) => {
+        console.log(food)
+        document.querySelector(`#${food.name}`).innerHTML += `<li>${instruction}</li>`;
+    }
+    const printLastItem = (instruction) => {
+        console.log(food)
+        document.querySelector(`#${food.name}`).innerHTML += `<li>${instruction}</li>`;
+        document.querySelector(`#${food.name}Img`).removeAttribute("hidden");
+    }
+
+    promises = []
+    for (let step = 0; step < food.steps; step++) {
+        if (step === food.steps - 1) {
+            promises.push(obtainInstruction(food.name, step).then(printLastItem).catch(printError));
+        } else {
+            promises.push(obtainInstruction(food.name, step).then(printItem).catch(printError));
+        }
+    }
+
+    await Promise.all(promises).catch((err) => console.log("catch()", err));
+})
